49d10d2787cbbfa0c6b24ac1bdac2528676b28bd
pyy233 <1244749150@qq.com>
2025-07-17 23:44:14 +0800

    Revert "Revert "Made a small change""
    This reverts commit 6c08346d6e0eb38746d90283db4e87ad870959a2.

--- frontend/src/pages/Home/index.jsx
+++ frontend/src/pages/Home/index.jsx
@@ -156,7 +156,8 @@ const HomePage = () => {
                       点击或拖拽文件到此区域上传
                     </p>
                     <p style={{ fontSize: '14px', color: '#666', margin: '0 0 8px 0' }}>
-                      支持ZIP格式，文件大小不能超过500MB
+                      支持ZIP格式，文件大小不能超过500MB<br/>
+                      仅支持lerobot dataset数据集
                     </p>
                   </div>
                 </div>
@@ -184,7 +185,8 @@ const HomePage = () => {
                       点击或拖拽模型文件到此区域上传
                     </p>
                     <p style={{ fontSize: '12px', color: '#666', margin: '0 0 4px 0' }}>
-                      支持ZIP格式，文件大小不能超过500MB（可选）
+                      支持ZIP格式，文件大小不能超过500MB（可选）<br/>
+                      仅支持URDF+STL格式模型文件
                     </p>
                   </div>
                 </div>
