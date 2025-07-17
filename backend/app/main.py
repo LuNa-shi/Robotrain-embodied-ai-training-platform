65ec39e3ad6b8ca79d0f7d323cf097eb6f885d49
Luna <shiwenxiang3525@126.com>
2025-07-17 03:01:17 +0000

    fix errors in cors on 5174

--- backend/app/main.py
+++ backend/app/main.py
@@ -33,7 +33,7 @@ from fastapi.middleware.cors import CORSMiddleware
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],  # 允许所有来源
+    allow_origins=["http://localhost:5173", "http://localhost:5174"],  # 允许所有来源
     allow_credentials=True,
     allow_methods=["*"],  # 允许所有方法
     allow_headers=["*"],  # 允许所有头部
